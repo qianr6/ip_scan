cba040342ffb89ae81c65900e3df09997b88770e
GatNing <742632713@qq.com>
2023-07-11 14:57:06 +0800

    fix：更换获取IP地址的接口

--- src/test/java/com/gatning/ip_scan/IpScanApplicationTests.java
+++ src/test/java/com/gatning/ip_scan/IpScanApplicationTests.java
@@ -1,15 +1,11 @@
 package com.gatning.ip_scan;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.gatning.ip_scan.entity.LocalIp;
 import com.gatning.ip_scan.service.LocalIpService;
 import com.gatning.ip_scan.utils.DDNS;
 import com.gatning.ip_scan.utils.IpScanUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.client.RestTemplate;
 
 import java.net.Inet6Address;
 import java.net.InetAddress;
@@ -23,18 +19,6 @@ class IpScanApplicationTests {
     @Autowired
     private IpScanUtils ipScanUtils;
 
-    @Autowired
-    private DDNS ddns;
-
-    @Autowired
-    private LocalIpService localIpService;
-
-
-//    @Test
-//    void contextLoads() {
-//        String ipAddress = ipScanUtils.getIpAddress();
-//        System.out.println(ipAddress);
-//    }
 
 
     @Test
@@ -65,21 +49,7 @@ class IpScanApplicationTests {
 
     @Test
     public void testIpConfig() {
-        RestTemplate restTemplate=new RestTemplate();
-            // url 自行填写
-            String url = "https://v6.myip.la/json";
-
-            // 发送get请求，并用String数据格式接收
-            String result = restTemplate.getForObject(url, String.class);
-
-            // 获取结果转 json
-            System.out.println(result);
-            JSONObject jsonObject =  JSON.parseObject(result);
-
-            // 转为json后，则可以根据json的键值取出value，
-            // jsonObject..get()中填写键值（key）
-            String value = (String) jsonObject.get("ip");
-            System.out.println(value);
-        }
+        String currentHostIP = ipScanUtils.getCurrentHostIP();
+    }
 
 }
